5e6a4f1a9fef62c5ba7bc30633c55ce136892dfd
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2019-09-12 23:07:35 -0300

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path: 'componentes', component: ComponentesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ComponentesComponent } from './componentes/componentes.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
